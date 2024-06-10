5aadc0720d349e44b0174abdf50a8ff701b7c812
Mike <mikesoares8@gmail.com>
2024-06-10 15:18:01 -0300

    instalando bootstrap e iniciando estilização

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SearchAddressComponent } from './components/pages/search-address/search-address.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SearchAddressComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
